eb30aee71ee4d4e4c15e9c26d7d1887d701ece0e
Adrian Radice <radice.adrian.sjt@gmail.com>
2018-07-15 00:23:08 -0300

    update libs

--- Sistema Embebido/Libs/Cubo.cpp
+++ Sistema Embebido/Libs/Cubo.cpp
@@ -3,37 +3,53 @@
 
 
 Cubo::Cubo(
-         DRV8825 &arriba,  DRV8825 &abajo,  DRV8825 &Lateral/*,
-            const int fwdPin,const  int revPin,const  int pasos,const  int steps, 
-            const int grados120,const  int grados90,const  int grados45, 
-            const int tiempoLectora,const  int pausa */
+            const int dirArr,    const int stepArr,   const int enblArr,
+            const int dirAbajo,  const int stepAbajo, const int enblAbajo,
+            const int dirCost,   const int stepCost,  const int enblCost,
+            const int fwdPin, const  int revPin, const int inputFinCarrera, const  int pasos,  
+            const int grados180, const  int grados90, const  int grados45, 
+            const int tiempoLectora, const  int pausa 
         )
 { 
-    /*this->fwdPin = fwdPin;
+    this->dirArr = dirArr;
+    this->stepArr = stepArr;
+    this->enblArr = enblArr;
+    this->dirAbajo = dirAbajo;
+    this->stepAbajo = stepAbajo;
+    this->enblAbajo = enblAbajo;
+    this->dirCost = dirCost;
+    this->stepCost = stepCost;
+    this->enblCost = enblCost;
+    this->fwdPin = fwdPin;
     this->revPin = revPin;
+    this->inputFinCarrera = inputFinCarrera;
     this->pasos = pasos;
-    this->steps = steps;
-    this->grados120 = grados120;
+    this->grados180 = grados180;
     this->grados90 = grados90;
-    this->grados45 = grados45;s
+    this->grados45 = grados45;
     this->tiempoLectora = tiempoLectora;
-    this->pausa = pausa;*/
+    this->pausa = pausa;
+    this->value = 0;
+
+    pinMode(fwdPin, OUTPUT);
+    pinMode(revPin, OUTPUT);
+    pinMode(inputFinCarrera, INPUT);
 }
-/*
+
 int Cubo::getFwdPin(){
     return this->fwdPin;
 }
 int Cubo::getRevPin(){
     return this->revPin;
 }
+int Cubo::getInputFinDeCarrera(){
+    return this->inputFinCarrera;
+}
 int Cubo::getPasos(){
     return this->pasos;
 }
-int Cubo::getSteps(){
-    return this->steps;
-}
-int Cubo::getGrados120(){
-    return this->grados120;
+int Cubo::getGrados180(){
+    return this->grados180;
 }
 int Cubo::getGrados90(){
     return this->grados90;
@@ -47,59 +63,200 @@ int Cubo::getTiempoLectora(){
 int Cubo::getPausa(){
     return this->pausa;
 }
-*/
+
 void Cubo::MovL(){
     Serial.println("MOVER L");
-}
+    //girar brazo costado 45 grados antihorario
+    //poner Brazo costado
+    //girar brazo costado 90 grados horario
+    //sacar brazo
+    //girar brazo costado 45 grados antihorario
+    
+    girarBrazoCostadoAntihorario(grados45);   
+    ponerBrazoCostado();   
+    girarBrazoCostadoHorario(grados90);
+    sacarBrazoCostado();   
+    girarBrazoCostadoAntihorario(grados45);
 
-void Cubo::MovR(){
-    Serial.println("MOVER R");
-}
-
-void Cubo::MovF(){
-    Serial.println("MOVER F");
-}
-
-void Cubo::MovB(){
-    Serial.println("MOVER B");
 }
 
 void Cubo::MovU(){
-    Serial.println("MOVER U");
-}
-void Cubo::MovD(){
-    Serial.println("MOVER D");
+    // pongo el brazo de costado con la mano horizontal; 
+    // giro arriba horario 90 grados;
+    // saco el brazo;
+    digitalWrite(dirArr, HIGH);  // Establezco una dirección
+    
+    ponerBrazoCostado();
+    
+    for (int x = 0; x < grados90 ; x++) { // giro arriba horario 90 grados;
+      digitalWrite(stepArr, HIGH);
+      delayMicroseconds(pausa);
+      digitalWrite(stepArr, LOW);
+      delayMicroseconds(pausa);
+    }
+    
+    sacarBrazoCostado(); 
 }
 
-void Cubo::MovRA(){
-    Serial.println("MOVER RA");
+void Cubo::MovD(){
+    Serial.println("MOVER D");
+    // pongo el brazo de costado con la mano horizontal; 
+    // giro abajo horario 90 grados;
+    // saco el brazo;
+    digitalWrite(dirAbajo, HIGH);  // Establezco una dirección
+    
+    ponerBrazoCostado();
+    
+    for (int x = 0; x < grados90 ; x++) {
+      digitalWrite(stepAbajo, HIGH);
+      delayMicroseconds(pausa);
+      digitalWrite(stepAbajo, LOW);
+      delayMicroseconds(pausa);
+    }
+    
+    sacarBrazoCostado(); 
 }
+
+void Cubo::MovX(){
+    // giro el motor de arriba y el motor de abajo 90grados en sentido horario
+    Serial.println("MOVER X");
+    digitalWrite(dirArr, HIGH);  // Establezco una dirección
+    digitalWrite(dirAbajo, HIGH);
+    
+    for (int x = 0; x < grados90 ; x++) {
+      digitalWrite(stepArr, HIGH); 
+      digitalWrite(stepAbajo, HIGH);
+      delayMicroseconds(pausa);
+      digitalWrite(stepArr, LOW);
+      digitalWrite(stepAbajo, LOW);
+      delayMicroseconds(pausa);
+    }
+}
+
 
 void Cubo::MovLA(){
     Serial.println("MOVER LA");
+    //girar brazo costado 45 grados horario
+    //poner Brazo costado
+    //girar brazo costado 90 grados antihorario
+    //sacar brazo
+    //girar brazo costado 45 grados horario
+    
+    girarBrazoCostadoHorario(grados45);   
+    ponerBrazoCostado();   
+    girarBrazoCostadoAntihorario(grados90);
+    sacarBrazoCostado();   
+    girarBrazoCostadoHorario(grados45);
 }
 
 void Cubo::MovUA(){
     Serial.println("MOVER UA");
+    // pongo el brazo de costado con la mano horizontal; 
+    // giro arriba antihorario 90 grados;
+    // saco el brazo;
+    digitalWrite(dirArr, LOW);  // Establezco una dirección
+    
+    ponerBrazoCostado();
+    
+    for (int x = 0; x < grados90 ; x++) { // giro arriba antihorario 90 grados;
+      digitalWrite(stepArr, HIGH);
+      delayMicroseconds(pausa);
+      digitalWrite(stepArr, LOW);
+      delayMicroseconds(pausa);
+    }
+
+    sacarBrazoCostado();
 }
 
 void Cubo::MovDA(){
     Serial.println("MOVER DA");
+    // pongo el brazo de costado con la mano horizontal; 
+                        // giro abajo antihorario 90 grados;
+                        // saco el brazo;
+    digitalWrite(dirAbajo, LOW);  // Establezco una dirección
+    
+    ponerBrazoCostado();
+    
+    for (int x = 0; x < grados90 ; x++) {
+      digitalWrite(stepAbajo, HIGH);
+      delayMicroseconds(pausa);
+      digitalWrite(stepAbajo, LOW);
+      delayMicroseconds(pausa);
+    }
+    
+    sacarBrazoCostado(); 
 }
 
-void Cubo::MovBA(){
-    Serial.println("MOVER BA");
+/*
+void Cubo::MovXA(){
+    Serial.println("MOVER XA");
+    // giro el motor de arriba y el motor de abajo 90grados en sentido antihorario    
+    digitalWrite(dirArr, LOW);  // Establezco una dirección
+    digitalWrite(dirAbajo, LOW);
+    
+    for (int x = 0; x < grados90 ; x++) {
+      digitalWrite(stepArr, HIGH);
+      digitalWrite(stepAbajo, HIGH);
+      delayMicroseconds(pausa);
+      digitalWrite(stepArr, LOW);
+      digitalWrite(stepAbajo, LOW);
+      delayMicroseconds(pausa);
+    } 
+}
+*/
+
+
+void Cubo::ponerBrazoCostado(){
+    
+    digitalWrite(revPin, HIGH);
+    digitalWrite(fwdPin, LOW);
+    
+    value = digitalRead(inputFinCarrera);
+    while( value == LOW ){
+
+      value = digitalRead(inputFinCarrera);
+    }
+    
+    digitalWrite(revPin, LOW);
+    digitalWrite(fwdPin, LOW);
+}
+  
+void Cubo::sacarBrazoCostado(){
+    digitalWrite(revPin, LOW);
+    digitalWrite(fwdPin, HIGH);
+    
+    delay(tiempoLectora);
+    
+    digitalWrite(revPin, LOW);
+    digitalWrite(fwdPin, LOW);
+}
+  
+void Cubo::girarBrazoCostadoHorario(int cantidadDePasos){
+    digitalWrite(dirCost, HIGH);  // Establezco una dirección
+    
+    for (int x = 0; x < cantidadDePasos ; x++) { // giro costado horario 90 grados;
+      digitalWrite(stepCost, HIGH);
+      delayMicroseconds(pausa);
+      digitalWrite(stepCost, LOW);
+      delayMicroseconds(pausa);
+    }
+}
+  
+void Cubo::girarBrazoCostadoAntihorario(int cantidadDePasos){
+    digitalWrite(dirCost, LOW);  // Establezco una dirección
+    
+    for (int x = 0; x < cantidadDePasos ; x++) { // giro costado horario 90 grados;
+      digitalWrite(stepCost, HIGH);
+      delayMicroseconds(pausa);
+      digitalWrite(stepCost, LOW);
+      delayMicroseconds(pausa);
+    }
 }
 
-void Cubo::MovFA(){
-    Serial.println("MOVER FA");
-}
 
 void Cubo::Mover( char c){
     switch(c){
-        case RC:
-            this->MovR();
-            break;
+
         case LC:
             this->MovL();
             break;
@@ -109,23 +266,8 @@ void Cubo::Mover( char c){
         case DC:
             this->MovD();
             break;
-        case FC:
-            this->MovF();
-            break;
-        case BC:
-            this->MovB();
-            break;
-        case RA:
-            this->MovRA();
-            break;
         case LA:
             this->MovLA();
-            break;
-        case FA:
-            this->MovFA();
-            break;
-        case BA:
-            this->MovBA();
             break;
         case UA:
             this->MovUA();
@@ -133,6 +275,13 @@ void Cubo::Mover( char c){
         case DA:
             this->MovDA();
             break;
+        case XC:
+            this->MovX();
+            break;
+       
+/*        case XA
+            this->MovXA();
+            break; */
     }
 
 }
--- Sistema Embebido/Libs/DRV8825.h
+++ Sistema Embebido/Libs/DRV8825.h
@@ -7,5 +7,13 @@ class DRV8825
 {
     public:
         DRV8825( int dir, int step, int enable );
+        int GetDir();
+        int GetStep();
+        int GetEnable();
+
+    private:
+        int dir;
+        int step;
+        int enable;
 };
 #endif
